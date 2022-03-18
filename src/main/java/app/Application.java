8e86ece9a7acc518034dbb2784cf1e37d2949be5
Denis <svd.denis@mail.ru>
2022-03-18 17:58:32 +0300

    2.1.3

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,7 +4,6 @@ import app.config.AppConfig;
 import app.model.AnimalsCage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class Application {
 
     public static void main(String[] args) {
